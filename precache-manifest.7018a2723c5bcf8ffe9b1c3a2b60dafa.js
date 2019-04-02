a675aa69f093823007f68c73dd1ecee6e023e0d2
Ilia Barkov <barkov@MacBook-Pro-Ilia.local>
2019-04-02 12:31:43 +0300

    Updates

---
+++ precache-manifest.7018a2723c5bcf8ffe9b1c3a2b60dafa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b867071ff6b861045f0a",
+    "url": "/cinema/static/js/runtime~main.069b81f7.js"
+  },
+  {
+    "revision": "73aa02e85f1d4c6d936a",
+    "url": "/cinema/static/js/main.e6962f67.chunk.js"
+  },
+  {
+    "revision": "dd01f7bc260fe3485618",
+    "url": "/cinema/static/js/2.827c1f48.chunk.js"
+  },
+  {
+    "revision": "73aa02e85f1d4c6d936a",
+    "url": "/cinema/static/css/main.b50da3af.chunk.css"
+  },
+  {
+    "revision": "23cad602975b5982c089aebbe168648f",
+    "url": "/cinema/index.html"
+  }
+];
